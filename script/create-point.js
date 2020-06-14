67b239bc6dd1e51005cd68014f9d891731778ff1
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-14 12:33:41 -0300

    fazer o teste da api IBGE estados no create-point.js

---
+++ script/create-point.js
@@ -0,0 +1,7 @@
+// retorna um objeto chamado promise
+fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(
+    function(res) { 
+        return res.json()
+    }).then(function(data){
+        console.log(data);
+    });
