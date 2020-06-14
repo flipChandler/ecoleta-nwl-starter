c47e4f385852c2ee93cecf79b0b70f51abdc4635
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-14 14:30:35 -0300

    select consumindo a api ibge cidade através do estado selecionado

--- script/create-point.js
+++ script/create-point.js
@@ -2,7 +2,7 @@ function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]");
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-        .then( res =>  res.json() ) // .then( (response) => { return response.json()} )
+        .then( response =>  response.json() ) // .then( (response) => { return response.json()} )
         .then( states => {
             for ( const state of states ) {
                 ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
@@ -11,6 +11,31 @@ function populateUFs() {
 }
 
 populateUFs(); // não se esqueça de chamar a função
+
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]");
+    const stateInput = document.querySelector("input[name=state]");
+
+    const ufValue = event.target.value;
+
+    const indexOfSelectedState = event.target.selectedIndex;
+    stateInput.value = event.target.options[indexOfSelectedState].text;
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+
+    fetch(url)
+        .then( response =>  response.json() ) // .then( (response) => { return response.json()} )
+        .then( cities => {
+            for ( const city of cities ) {
+                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            }
+
+            citySelect.disabled = false;
+        });
+}
+
+
 
 
 
@@ -22,10 +47,7 @@ populateUFs(); // não se esqueça de chamar a função
 
  document
     .querySelector("select[name=uf]") // procure o select que tem o name uf
-    .addEventListener("change", () => {
-        console.log("mudei");
-    })
-
+    .addEventListener("change", getCities)
 /*     // retorna um objeto chamado promise
 fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(
     function(res) { 
