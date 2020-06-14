19abc348bba4e1a0b8b83e35171401b1c06b60dc
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-14 12:40:10 -0300

    fazer o teste do querySelector | mudar option do select no create-point.html

--- script/create-point.js
+++ script/create-point.js
@@ -1,3 +1,8 @@
+document
+    .querySelector("select[name=uf]") // procure o select que tem o name uf
+    .addEventListener("change", () => {
+        console.log("mudei");
+    })
 // retorna um objeto chamado promise
 fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(
     function(res) { 
