d2a2a1b3c5adeb46a81a67d4e13472e61708968c
Felipe Santos <felipe.santos11@hotmail.com>
2020-06-14 13:47:40 -0300

    select consumindo a api ibge estado

--- script/create-point.js
+++ script/create-point.js
@@ -1,12 +1,35 @@
-document
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]");
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then( res =>  res.json() ) // .then( (response) => { return response.json()} )
+        .then( states => {
+            for ( const state of states ) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        });
+}
+
+populateUFs(); // não se esqueça de chamar a função
+
+
+
+
+
+
+
+
+
+ document
     .querySelector("select[name=uf]") // procure o select que tem o name uf
     .addEventListener("change", () => {
         console.log("mudei");
     })
-// retorna um objeto chamado promise
+
+/*     // retorna um objeto chamado promise
 fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados").then(
     function(res) { 
         return res.json()
     }).then(function(data){
         console.log(data);
-    });
+    }); */
